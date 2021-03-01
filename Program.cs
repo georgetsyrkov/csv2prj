839555b31bc7f6c3d69854543a09ca3f67c5ca25
George <george.tsyrkov@gmail.com>
2021-03-01 17:43:51 +0300

    csv

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace csv2prj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                System.IO.FileInfo fi = new System.IO.FileInfo(args[0]);
+                if (fi.Exists)
+                {
+                    MainParser.ParseCSV(fi.FullName);
+                }
+                else
+                {
+                    Console.WriteLine("Файл не существует");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Необходимо указать имя CSV файла для работы");
+            }
+            
+        }
+    }
+}
---
+++ mainParser.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace csv2prj
+{
+    public static class MainParser
+    {
+        public static void ParseCSV(string csvFile)
+        {
+
+        }
+    }
+}
